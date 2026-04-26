3b4b828ba6aaa3c551481915d4dbf5d90807e01d
Juhn06 <nguyenthixuan10062005@gmail.com>
2026-04-27 01:14:19 +0700

    dean

--- models/init.py
+++
@@ -1,9 +0,0 @@
-"""
-Models package
-Export all models
-"""
-from models.user import User
-from models.document import Document
-from models.reading_session import ReadingSession
-
-__all__ = ['User', 'Document', 'ReadingSession']
--- utils/init.py
+++
@@ -1,8 +0,0 @@
-"""
-Utils package
-Utility functions
-"""
-from utils.decorators import admin_required
-from utils.validators import validate_username, validate_email, validate_password
-
-__all__ = ['admin_required', 'validate_username', 'validate_email', 'validate_password']
